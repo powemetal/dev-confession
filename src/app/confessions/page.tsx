c76e07a50f712a62c9efdfe6ae00be7e90c1c2f5
powemetal <francisbvrt@gmail.com>
2026-09-03 11:51:29 -0400

    ajout page confessions

--- src/app/confessions/page.tsx
+++ src/app/confessions/page.tsx
@@ -1,1 +1,12 @@
 // Lister - Afficher - Filtrer - Pagination des confessions
+export default async function ConfessionsPage(){
+    return(
+        <div className="min-h-screen bg-gray-900 via-purple-900/20 to-gray-900">
+                <p>
+                    
+                    Hello
+                </p>
+        </div>
+    );
+}
+ 
